83d26fe3013071d0a5f10b4e503542558d104b49
ScAziz <stephen.aziz@hotmail.co.uk>
2023-02-21 11:27:28 +0000

    insert session middlewre into app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require("express-session");
 
 var homeRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(methodOverride('_method'));
+
+app.use(
+  session({
+    key: "user_sid",
+    secret: "super_secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      expires: 600000,
+    },
+  })
+);
+
+// clear the cookies after user logs out
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie("user_sid");
+  }
+  next();
+});
+
+// middleware function to check for logged-in users
+const sessionChecker = (req, res, next) => {
+  if (!req.session.user && !req.cookies.user_sid) {
+    console.log('redirect to login');
+    res.redirect("/sessions/new");
+  } else {
+    next();
+  }
+};
 
 app.use('/', homeRouter);
 app.use('/user', usersRouter);
