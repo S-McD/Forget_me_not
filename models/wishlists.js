ceb15bc64b82aa4b4a9def1b276facd0df1b428f
ScAziz <stephen.aziz@hotmail.co.uk>
2023-02-27 14:52:55 +0000

    amend wishlist model

--- models/wishlists.js
+++ models/wishlists.js
@@ -4,6 +4,7 @@ const WishlistSchema = new mongoose.Schema({
   name: String,
   members: [{type: mongoose.Types.ObjectId, ref: "User"}],
   gifts: [{type: mongoose.Types.ObjectId, ref: "Gift"}],
+  creator: {type: mongoose.Types.ObjectId, ref: "User"},
 });
   
 const Wishlist = mongoose.model("wishlists", WishlistSchema);
