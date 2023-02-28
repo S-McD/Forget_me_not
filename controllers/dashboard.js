0fe8256ed3d13d10a0d40bf4a547557fa0d324c3
Simone <95197307+S-McD@users.noreply.github.com>
2023-02-28 00:18:28 +0000

    updated the user dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,11 +1,14 @@
 const Event = require("../models/events");
+const Request= require("../models/requests");
 
 const DashboardController = {
 
 Index: async (req, res) => {
+    const user = req.session.user;
     const userEvents = await Event.find({ creator: req.session.user._id }).sort({date: 1});
-    res.render("userdashboard", { events: userEvents });
+    const userRequests = await Request.find({ recipient: user._id }).where({ status: 'pending'});
+    res.render("userdashboard", { user: user, events: userEvents, requests: userRequests });
   },
 };
 
-module.exports = DashboardController;  
+module.exports = DashboardController;
--- models/requests.js
+++ models/requests.js
@@ -4,7 +4,7 @@ const RequestSchema = new mongoose.Schema({
   creator: {type: mongoose.Types.ObjectId, ref: "User"},
   recipient: {type: mongoose.Types.ObjectId, ref: "User"},
   event: {type: mongoose.Types.ObjectId, ref: "Event"},
-  status: String,
+  status: [String],
 });
   
 const Request = mongoose.model("Request", RequestSchema);
