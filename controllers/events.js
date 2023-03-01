90c5082dc1a0e0d3febef0c3a1dac1844e37b5c7
Simone <95197307+S-McD@users.noreply.github.com>
2023-03-01 15:42:43 +0000

    added event id to user

--- controllers/events.js
+++ controllers/events.js
@@ -1,30 +1,35 @@
 const Event = require("../models/events");
-
+const User = require("../models/users");
 
 const EventsController = {
-    Index: async (req, res) => {
-      const userEvents = await Event.find({ creator: req.session.user._id });
-      res.render("events", { events: userEvents });
-    },
+  Index: async (req, res) => {
+    const userEvents = await Event.find({ creator: req.session.user._id });
+    res.render("events", { events: userEvents });
+  },
 
-    New: (req, res) => {
-      res.render("new_event");
-      },
+  New: (req, res) => {
+    res.render("new_event");
+  },
 
-    Create: (req, res) => {
-      console.log("I'm creating an event")
-      const event = new Event(req.body);
-      event.creator = req.session.user._id;
-      event.save((err) => {
-          if (err) {
-            throw err;
+  Create: (req, res) => {
+    console.log("I'm creating an event")
+    const event = new Event(req.body);
+    event.creator = req.session.user._id;
+    event.save((err) => {
+      if (err) {
+        throw err;
+      } else {
+        User.findOneAndUpdate({ _id: req.session.user._id }, { $push: { event_IDs: event._id } },
+          function (error, success) {
+          if (error) {
+              console.log(error);
           } else {
-            res.redirect("/dashboard/userdashboard")
-          }
-      });
-      },
-    };
+              console.log(success);
+          } }, )
+      }});  
+      res.redirect("/events");
+  },
+};
   
-  
-  module.exports = EventsController;
+module.exports = EventsController;
   
