b1303959ca102fe8ce6cc7badfb749c32bcd44f6
ScAziz <stephen.aziz@hotmail.co.uk>
2023-03-02 11:46:32 +0000

    build decline function

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -118,47 +118,23 @@ const WishlistController = {
         }
       },
 
-        // Invite: (req, res) => {
-        //   console.log("finding user");
-        //   const first_name = req.body.first_name;
-        //   // const last_name = req.body.last_name
-        //   User.findOne ({ first_name: first_name }).then((user) => {
-        //     if (!user) {
-        //       res.render("invite", {error: "user doesn't exist"});
-        //       console.log("ERROR");
-        //     } else {
-    
-        //       Wishlist.findOneAndUpdate({ _id: req.params.wishlistID }, 
-        //         { $push: { members: user._id } },
-        //         function (error, success) {
-        //           if (error) {
-        //               console.log(error);
-        //           } else {
-        //               console.log(success);
-        //           }
-        //       });
-        
-        //       const wishlist = new Wishlist(
-        //          );
-        //       wishlist.creator = req.session.user;
-        //       wishlist.members = user._id;
-        //       // wishlist.event = req.params.eventID; 
-        //       wishlist.save((err) => {
-        //       if (err) {
-        //         throw err;
-        //       }
-        //       console.log(wishlist)
-    
-        //       // update event invitees
-        //       // create new request object and save it 
-        //       // redirect 
-    
-        //       console.log(user);
-        //       console.log("got em");
-        //       res.status(201).redirect("/wishlist");
-        //     })
-        //   }});
-        // },
+      Decline: (req, res) => {
+        var success = Object.assign({},req.body)
+        console.log(success)
+        console.log(req.body)
+        if (success.response == "rejected") {
+          console.log(req.session.user._id)
+        Request.findOneAndUpdate({ recipient: req.session.user._id }, 
+          { status: "rejected" },
+          function (error, success) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(success);
+            }
+          });
+          res.redirect("/requests")
+      },
 };
 
 module.exports = WishlistController;
--- routes/wishlists.js
+++ routes/wishlists.js
@@ -9,9 +9,7 @@ router.get("/wishlist_invite/:wishlistID", WishlistController.Wishlist);
 router.post("/new_wishlist", WishlistController.Create);
 router.get("/wishlist_invite/:wishlistID", WishlistController.Invite);
 router.post("/wishlist_invite/:wishlistID/accept", WishlistController.Accept);
-// router.post("/wishlist_invite/:wishlistID/decline", WishlistController.Decline);
-
-// router.post("/wishlist_invite/:wishlistID", WishlistController.Invite);
+router.post("/wishlist_invite/:wishlistID/decline", WishlistController.Decline);
 
 
 module.exports = router;
