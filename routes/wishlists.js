2ebc6dabbb1b11f4d57acd94ec679188e1d39443
ScAziz <stephen.aziz@hotmail.co.uk>
2023-03-02 11:59:17 +0000

    update wishlist controller

--- routes/wishlists.js
+++ routes/wishlists.js
@@ -8,6 +8,7 @@ router.get("/new_wishlist", WishlistController.New);
 router.get("/wishlist_invite/:wishlistID", WishlistController.Wishlist);
 router.post("/new_wishlist", WishlistController.Create);
 router.get("/wishlist_invite/:wishlistID", WishlistController.Invite);
+router.post("/assignEvent/:wishlistID", WishlistController.Assign);
 router.post("/wishlist_invite/:wishlistID/accept", WishlistController.Accept);
 router.post("/wishlist_invite/:wishlistID/decline", WishlistController.Decline);
 
